2fdaec393fe3710f7b055e1e47757978e1de693d
harivmasoor <harimasoor@gmail.com>
2023-05-10 18:59:21 -0700

    force push

--- HabitMaster/Views/AddEditHabit/AddEditHabitView.swift
+++ HabitMaster/Views/AddEditHabit/AddEditHabitView.swift
@@ -3,18 +3,21 @@ import SwiftUI
 struct AddEditHabitView: View {
     @Environment(\.presentationMode) private var presentationMode
     @EnvironmentObject var viewModel: HabitListViewModel
-    
+
     let habitToEdit: Habit?
     @State private var habit: Habit
-    
+
     // Add a state variable for the selected completion date
     @State private var selectedCompletionDate = Date()
-    
+
     init(habitToEdit: Habit? = nil) {
         self.habitToEdit = habitToEdit
         _habit = State(initialValue: habitToEdit ?? Habit(name: "", subtitle: ""))
+        if let habitToEdit = habitToEdit {
+            _selectedCompletionDate = State(initialValue: habitToEdit.completionDate)
+        }
     }
-    
+
     var body: some View {
         NavigationView {
             Form {
@@ -22,7 +25,7 @@ struct AddEditHabitView: View {
                     TextField("Habit Name", text: $habit.name)
                     // Replace "Habit Subtitle" with "Description"
                     TextField("Description", text: $habit.subtitle)
-                    
+
                     // Add a DatePicker for selecting the completion date
                     DatePicker("Completion Date", selection: $selectedCompletionDate, displayedComponents: .date)
                 }
@@ -41,15 +44,17 @@ struct AddEditHabitView: View {
                             var updatedHabit = habit
                             updatedHabit.subtitle = habit.subtitle
                             updatedHabit.completionDates.append(selectedCompletionDate)
+                            updatedHabit.completionDate = selectedCompletionDate // Add this line
                             viewModel.habits[index] = updatedHabit
                         } else {
                             // Create a new habit with the selected completion date and edited description
                             var newHabit = habit
                             newHabit.subtitle = habit.subtitle
                             newHabit.completionDates.append(selectedCompletionDate)
+                            newHabit.completionDate = selectedCompletionDate // Add this line
                             viewModel.habits.append(newHabit)
                         }
-                        
+
                         presentationMode.wrappedValue.dismiss()
                     }
                 }
@@ -57,6 +62,7 @@ struct AddEditHabitView: View {
         }
     }
 }
+
  
 
 
